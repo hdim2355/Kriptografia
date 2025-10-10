7fdf80add6600990f099436c1f5ee73e65ef4d2d
hdim2355 <davidholub10@gmail.com>
2025-10-10 14:56:25 +0200

    Scytale Cipher and Railfence Cipher encrypt + decrypt

--- lab1/cezar.py
+++ lab1/cezar.py
@@ -1,10 +1,3 @@
-# ascii_256 = ''.join(chr(i) for i in range(256))
-# print(ascii_256)
-# print(f"Hossz: {len(ascii_256)}")  # 256
-# print(f"Első 32 karakter (vezérlőkarakterek): {ascii_256[:32]!r}")
-# print(f"Nyomtatható karakterek: {ascii_256[32:127]}")
-
-
 def encrypt_scytale(plaintext, circumference): #5
     # plaintext.upper()
     while len(plaintext) % circumference != 0:
@@ -14,11 +7,71 @@ def encrypt_scytale(plaintext, circumference): #5
     for i in range(circumference):
         for j in range(cols):
             output += plaintext[j * circumference + i]
-    print(output)
+    return output
 
-plaintext = input()
-encrypt_scytale(plaintext, 4) # eddig megy a decriptre is
+def decrypt_scytale(ciphertext, circumference):
+    while len(ciphertext) % circumference != 0:
+        ciphertext += "X"
+    cols = len(ciphertext) // circumference
+    output = ""
+    for i in range(circumference-1,0,-1):
+        for j in range(cols,-1,-1):
+            output += ciphertext[j * cols + i-1]
+    return output[::-1]
 
-def encrypt_railfence(val,n):
-    m = n-2
-    next = n + m
+# print(encrypt_scytale("IAMHURTVERYBADLYHELP", 5))
+# print(decrypt_scytale("IRYYATBHMVAEHEDLURLP",5))
+
+def encrypt_railfence(plaintext,rails):
+    lists = [[] for _ in range(rails)]
+    listIndex = 0
+    direction = 1
+    for char in plaintext:
+        lists[listIndex].append(char)
+        if listIndex == 0:
+            direction = 1
+        elif listIndex == rails - 1:
+            direction = -1
+        listIndex += direction
+    output = ""
+    for list in lists:
+        for char in list:
+            output += char
+    return output
+
+# print(encrypt_railfence("WEAREDISCOVEREDFLEEATONCE", 3))
+def decrypt_railfence(ciphertext, rails):
+    rail_lengths = [0] * rails
+    listIndex = 0
+    direction = 1
+
+    for _ in range(len(ciphertext)):
+        rail_lengths[listIndex] += 1
+        if listIndex == 0:
+            direction = 1
+        elif listIndex == rails - 1:
+            direction = -1
+        listIndex += direction
+    lists = [[] for _ in range(rails)]
+
+    index = len(ciphertext) - 1
+    for rail in range(rails - 1, -1, -1):
+        for _ in range(rail_lengths[rail]):
+            lists[rail].append(ciphertext[index])
+            index -= 1
+
+    output = ""
+    listIndex = 0
+    direction = 1
+
+    for _ in range(len(ciphertext)):
+        output += lists[listIndex].pop()
+        if listIndex == 0:
+            direction = 1
+        elif listIndex == rails - 1:
+            direction = -1
+        listIndex += direction
+
+    return output
+
+print(decrypt_railfence("WECRLTEERDSOEEFEAOCAIVDEN",3))
